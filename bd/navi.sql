2a193a56a472c4e091e2afcd41a3377930d2ef28
lilo-error404 <murilosouzagames2005@gmail.com>
2025-09-18 10:59:53 -0300

    Update - BD

--- bd/navi.sql
+++ bd/navi.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE navi;
+USE navi;
 
 CREATE TABLE estacionamento (
 id_estacionamento INT AUTO_INCREMENT PRIMARY KEY,
@@ -19,7 +20,7 @@ FOREIGN KEY (id_estacionamento) REFERENCES estacionamento (id_estacionamento)
 );
 
 CREATE TABLE usuario (
-id_usario INT AUTO_INCREMENT PRIMARY KEY,
+id_usuario INT AUTO_INCREMENT PRIMARY KEY,
 nome VARCHAR(255) NOT NULL,
 email VARCHAR(300) NOT NULL,
 senha VARCHAR(12) NOT NULL,
@@ -40,11 +41,11 @@ CREATE TABLE reserva (
 id_reserva INT AUTO_INCREMENT PRIMARY KEY,
 id_usuario INT NOT NULL,
 id_vaga INT NOT NULL,
-data_hora_inicio TIMESTAMP,
-data_hora_fim TIMESTAMP,
+data_hora_inicio TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+data_hora_fim TIMESTAMP NULL,
 status ENUM('reservado', 'aguardando pagamento', 'cancelado'),
 FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
-FOREIGN KEY (id_vaga) REFERENCES vaga(id_vaga)
+FOREIGN KEY (id_vaga) REFERENCES vagas(id_vaga)
 );
 
 CREATE TABLE pagamento (
@@ -54,3 +55,21 @@ valor decimal (10,2),
 metodo ENUM('pix', 'debito', 'credito'),
 data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+#-----------------------------------------------------------------------------------
+#ideia de add avaliações
+
+/*
+CREATE TABLE avaliacao (
+id_avalicao INT AUTO_INCREMENT PRIMARY KEY,
+id_usuario INT NOT NULL,
+data_postado TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+comentário VARCHAR(500),
+FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+);
+*/
+
+#------------------------------------------------------------------------------------
+#dropar o bagui se dar erro
+
+#drop database navi;
