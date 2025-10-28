ced874bb0f6ecf7e4b7b700a0657e9f3c0cf5d1e
YZNYC <nycolasdelimaleandro@gmai]l.com>
2025-10-28 16:45:23 -0300

    Structure | Pastas reestruturas nos omponents para melhor compreensão

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -1,6 +1,6 @@
-import Header from "@/components/navbar/page";
-import Hero3 from "../components/banner/page"
-import Navbar04Page from "@/components/navbar-04/navbar-04";
+import Header from "@/components/shadcn/navbar/page";
+import Hero3 from "../components/landingpage/banner/page"
+import Navbar04Page from "@/components/shadcn/navbar-04/navbar-04";
 import { Footer7 } from "@/components/footer/footer-7";
 import FeaturesSection from '../components/teste/FeaturesSection';
 import ShowcaseSection from '../components/teste/ShowcaseSection';
